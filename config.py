18fdfb3d0f959175a525fa72ef0ef8a1f0f660ed
TITAN COMMUNITY <153067412+TITANASSOCIATION@users.noreply.github.com>
2024-05-27 08:45:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,10 @@
-#(©)CodeXBotz
-
-
-
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-
-#Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", ""))
-
-#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
